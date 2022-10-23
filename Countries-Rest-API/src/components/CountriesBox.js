f5ac565730b6d1f7cf9b858ffae4d42c41bb1f0c
Hiticas Mihai <hiticasmihai7@gmail.com>
2022-10-24 01:53:14 +0300

    extra

--- Countries-Rest-API/src/components/CountriesBox.js
+++ Countries-Rest-API/src/components/CountriesBox.js
@@ -4,7 +4,7 @@ import CountryBox from "./CountryBox";
 const CountriesBox = ({ countries, openCountryDescription }) => {
     return (
         <div className="content w-full flex justify-center p-5 lg:p-0 lg:mt-10">
-            <div className="grid w-full gap-10 grid-cols-1 grid-flow-raw sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+            <div className="grid w-full gap-10 grid-cols-1 grid-flow-raw sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 xl:gap-16">
                 {countries.length === 0 ? (
                     <p>Countries fetching...</p>
                 ) : (
