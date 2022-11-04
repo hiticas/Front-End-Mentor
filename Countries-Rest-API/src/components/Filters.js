bbc7bad63fe4c5feaa28fc41a777bc4a4dc27d55
Hiticas Mihai <hiticasmihai7@gmail.com>
2022-11-04 18:57:18 +0200

    fix frontend mentor warnings p4

--- Countries-Rest-API/src/components/Filters.js
+++ Countries-Rest-API/src/components/Filters.js
@@ -20,9 +20,11 @@ const Filters = ({ handleSearch, handleDropdown }) => {
                 />
             </div>
             <div className="dropdown h-14">
+                <label for="regions"></label>
                 <select
                     className="h-full p-2 rounded-md bg-[var(--header-color)] text-gray-200 text-sm border shadow-[0_0_7px_rgba(0,0,0,0.3)] border-transparent focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
                     defaultValue={"all"}
+                    name="regions"
                     id="regions"
                     onChange={(event) => handleDropdown(event.target.value)}
                 >
