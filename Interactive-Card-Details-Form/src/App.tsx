6c2823899854e317023f7ab54ca91c5e6f2e7581
hiticas <hiticasmihai7@gmail.com>
2023-09-26 02:17:25 +0300

    add project assets, structure app comp

--- Interactive-Card-Details-Form/src/App.tsx
+++ Interactive-Card-Details-Form/src/App.tsx
@@ -2,8 +2,20 @@ import './App.scss'
 
 function App() {
 	return (
-		<div className="card">
-			<h1>Interactive Card Details Form</h1>
+		<div className="app">
+			<div className='cards'>
+				<div className='cardA'>
+					<h1>cardA</h1>
+				</div>
+				<div className='cardB'>
+					<h1>cardB</h1>
+				</div>
+			</div>
+			<div className='form'>
+				<div className='form_title'>
+					<h1>Form Section</h1>
+				</div>
+			</div>
 		</div>
 	)
 }
