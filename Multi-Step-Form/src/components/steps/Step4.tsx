ee5cf7a8ed5869782d4398bf080308f4ae39fd7d
Mihai Hiticas <hiticasmihai7@gmail.com>
2024-03-24 02:55:12 +0200

    add functionality for step 4 with total cost

--- Multi-Step-Form/src/components/steps/Step4.tsx
+++ Multi-Step-Form/src/components/steps/Step4.tsx
@@ -14,35 +14,127 @@ interface CompleteProps {
 }
 
 const Complete: React.FC<CompleteProps> = ({ formData, onStepClick }) => {
+  const getPlanValue = (plan: string, subscription: string): number => {
+    let baseValue = 0;
+    switch (plan) {
+      case "arcade":
+        baseValue = 9;
+        break;
+      case "advance":
+        baseValue = 12;
+        break;
+      case "pro":
+        baseValue = 15;
+        break;
+    }
+    return subscription === "yearly" ? baseValue * 10 : baseValue;
+  };
+
+  const planValue = getPlanValue(formData.plan, formData.subscription);
+
+  const getAdditionalCostValue = (
+    service: string,
+    subscription: string
+  ): number => {
+    switch (service) {
+      case "onlineServices":
+        return subscription === "monthly" ? 1 : 10;
+      case "largeStorage":
+      case "customizableProfile":
+        return subscription === "monthly" ? 2 : 20;
+      default:
+        return 0;
+    }
+  };
+
+  const getTotalCost = (
+    onlineServices: boolean,
+    largeStorage: boolean,
+    customizableProfile: boolean
+  ): number => {
+    let totalCost = getPlanValue(formData.plan, formData.subscription);
+
+    if (onlineServices) {
+      totalCost += getAdditionalCostValue(
+        "onlineServices",
+        formData.subscription
+      );
+    }
+    if (largeStorage) {
+      totalCost += getAdditionalCostValue(
+        "largeStorage",
+        formData.subscription
+      );
+    }
+    if (customizableProfile) {
+      totalCost += getAdditionalCostValue(
+        "customizableProfile",
+        formData.subscription
+      );
+    }
+
+    return totalCost;
+  };
+
+  const totalCost = getTotalCost(
+    formData.onlineServices,
+    formData.largeStorage,
+    formData.customizableProfile
+  );
+
   return (
     <div>
       <h1 className="font-bolder">Complete</h1>
-      <div className="">
-        <div className="flex">
-          <h3>{formData.plan}</h3>
-          <h3>({formData.subscription})</h3>
+      <div className="w-full flex justify-around">
+        <div className="">
+          <div className="flex">
+            <h3>{formData.plan}</h3>
+            <h3>({formData.subscription})</h3>
+          </div>
+          <button className="border-2" onClick={() => onStepClick(2)}>
+            Change
+          </button>
         </div>
-        <button className="border-2" onClick={() => onStepClick(2)}>
-          Change
-        </button>
+        <div className="plan-details">
+          {planValue}/{formData.subscription === "monthly" ? "mo" : "yr"}
+        </div>
       </div>
-      <ul className="list-disc">
+      <div className="">
         {formData.onlineServices && (
-          <li>
+          <div>
             <h3>Online Services</h3>
-          </li>
+            <h3>
+              +{getAdditionalCostValue("onlineServices", formData.subscription)}
+              /{formData.subscription === "monthly" ? "mo" : "yr"}
+            </h3>
+          </div>
         )}
         {formData.largeStorage && (
-          <li>
+          <div>
             <h3>Large Storage</h3>
-          </li>
+            <h3>
+              +{getAdditionalCostValue("largeStorage", formData.subscription)}/
+              {formData.subscription === "monthly" ? "mo" : "yr"}
+            </h3>
+          </div>
         )}
         {formData.customizableProfile && (
-          <li>
+          <div>
             <h3>Customizable Profile</h3>
-          </li>
+            <h3>
+              +
+              {getAdditionalCostValue(
+                "customizableProfile",
+                formData.subscription
+              )}
+              /{formData.subscription === "monthly" ? "mo" : "yr"}
+            </h3>
+          </div>
         )}
-      </ul>
+      </div>
+      <div>
+        <h1 className="font-bold">Total: {totalCost}</h1>
+      </div>
     </div>
   );
 };
